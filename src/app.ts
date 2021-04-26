9dad808299e8a613feb182e202fa4dfe75506625
Heng Eng <heng.c.eng@gmail.com>
2021-04-26 14:49:02 +0800

    Takes no action if IP address (of host in hostname parameter has not changed.

--- src/app.ts
+++ src/app.ts
@@ -5,34 +5,62 @@
  * Imitates a Dyn DNS server. Some routers, eg D-Link, allow the specification of a server that handles the same protocol as Dyn DNS.
  * The router calls the specified server when it detects an IP address change.
  * 
- * This server handles such requests and update an entrydns.org dyanamic host with the IP address of the requester. Modify the 
- * update part as necessary to suit your DNS server you are using.
+ * This server handles such requests and updates an entrydns.org dyanamic host with the IP address of the requester. Modify the 
+ * update part as necessary to suit the dynamic DNS server you are using.
  * 
- * GET request:  /update?host=unused with Basic authentication credentials containing the EntryDNS token in the Password field.
+ * API: GET /update?host=notused with Basic authentication credentials containing the EntryDNS Token in the Password field.
  * 
- * The requester's IP address is assumed to be the address to be udpated.
+ * The requester's IP address is the address to be usess for updating the domain. This version assumes the server is behind a proxy and the
+ * proxy passes the real IP address in the header `x-real-ip`. Modify accordingly (eg to req.socket.remoteAddress) to suit your need.
  * 
- * Server than GET https://entrydns.org/records/modify/<token>?ip=....
+ * After data is validated, server then GET https://entrydns.org/records/modify/Token?ip=<IPv4 address> (See https://entrydns.net/help)
  * 
- */
-
+ ****************************************/
 
 console.log(Date(), "App loading...");
 import http from "http";
 import https from "https";
+import dns from "dns";
+import querystring from "querystring";
 
-const Server = http.createServer((req, res) => {
+const resolveDNS = (host:string) => {
+  return new Promise((resolve: (addr: string) => void, reject) => {
+    dns.lookup(host, 4, (err, addr) => {
+      if (err)
+        reject(err);
+      else
+        resolve(addr);
+    })
+  })
+} 
+
+const updateEntryDNS = (token:string, ip:string) => {
+  const updateUrl = `https://entrydns.net/records/modify/${token}?ip=${ip}`;
+  return new Promise((resolve: (statusCode:number) => void, reject) => {
+    https.get(updateUrl, res => {
+      console.log(`${updateUrl} result is: ${res.statusCode}`);
+      res.on("data", d => { console.log(`Response from EntryDNS: ${d.toString("ascii")}`); });
+      res.on("end", () => {resolve(res.statusCode as number);});
+    }).on("error", e => {
+      console.log(`Error from entrydns:`, e);
+    })
+})
+}
+
+
+/********* Main Application Loop *************/
+const Server = http.createServer(async (req, res) => {
   if (req.url && req.url?.indexOf("/update")<0) {
     res.writeHead(404);
     return res.end();
   }
   console.log("**** ", new Date(), ` ${req.url} ${req.socket.remoteAddress}`);
 
-  let statusCode = 500;
+  let statusCode = 500, rc = "911";
   const c_IP = "x-real-ip", c_auth = "authorization";
 
   /** Get the IP address */
-  let realIP = "";
+  let realIP: string;
   if (Array.isArray(req.headers[c_IP]))
     realIP = req.headers[c_IP]?.[0] ?? "";
   else
@@ -40,51 +68,82 @@ const Server = http.createServer((req, res) => {
   console.log(`IP is ${realIP}`);
 
   if (!realIP) {
-    console.error(Date(), `x-real-IP missing`);
-    console.log( `x-real-IP missing`);
-    statusCode = 402;
+    console.error(new Date(), `x-real-ip missing`);
+    console.log(`x-real-ip missing`);
+    statusCode = 402; rc = "nofqdn";
   } else {
-
-    /** Get credentials */
-    let credentials = ["", ""];
-    if (!req.headers[c_auth]) {
-      console.error(Date(), `authorization missing`);
-      statusCode = 401;
+    /* Get the host parameter */
+    let host: string | string[] | undefined = undefined;
+    const q = req.url?.split("?");
+    if (q?.length == 2) {
+      host = querystring.decode(q[1]).hostname;
+      if (Array.isArray(host)) host = host[0];
+    }
+    console.log(`host is ${host}`);
+    /* See if IP address has changed from current DNS value */
+    let currentIP = "";
+    if (host) {
+      try {
+        currentIP = await resolveDNS(host);
+        console.log(`${host} is currently ${currentIP}`);
+      } catch (err) {
+        console.error(new Date(), "resolveDNS error", err);
+      }
+    }
+    if (currentIP == realIP) {
+      statusCode = 200; rc = "nochg";
+      console.log("IP address has not changed. Not updating.")
     } else {
-      console.log(`authorization:${req.headers.authorization}`)
-      const parts = req.headers[c_auth]?.split("Basic ");
-      if (parts?.length != 2) {
-        console.error(Date(), ` authorization is not Basic`);
-        statusCode = 403;
+      /** Get credentials */
+      let credentials = ["", ""];
+      if (!req.headers[c_auth]) {
+        console.error(new Date(), `authorization missing`);
+        statusCode = 401; rc = "badauth";
       } else {
-        const buff = Buffer.from(parts[1], "base64");
-        const c = buff.toString("ascii");
-        credentials = c.split(":");
-        if (credentials.length != 2) {
-          console.error(Date(), ` Basic credentials format error: ${req.headers.authorization}=${c}`);
-          statusCode = 406;
+        console.log(`authorization:${req.headers.authorization}`)
+        const parts = req.headers[c_auth]?.split("Basic ");
+        if (parts?.length != 2) {
+          console.error(new Date(), ` authorization is not Basic`);
+          statusCode = 403; rc = "badauth";
         } else {
-          console.log(`Credentials: ${credentials}`);
-          const updateUrl = `https://entrydns.net/records/modify/${credentials[1]}?ip=${realIP}`;
-          https.get(updateUrl, res => {
-            console.log(`${updateUrl} result is: ${res.statusCode}`);
-            res.on("data", d => { console.log(d.toString("ascii")); });
-          }).on("error", e => {
-            console.log(`Error from entrydns:`, e);
-          })
+          const buff = Buffer.from(parts[1], "base64");
+          const c = buff.toString("ascii");
+          credentials = c.split(":");
+          if (credentials.length != 2) {
+            console.error(new Date(), ` Basic credentials format error: ${req.headers.authorization}=${c}`);
+            statusCode = 406; rc = "badauth";
+          } else {
+            console.log(`Credentials: ${credentials}`);
+            /*******************************************
+             * Updating a dynamic host in entrydns.org
+             ******************************************/
+            await updateEntryDNS(credentials[1], realIP).then(
+              s => {
+                statusCode = s; 
+                rc = s == 200 ? "good" : "Unexpected return from EntryDNS";
+              }
+            ).catch(
+              err => {
+                console.log(`Error return from calling EntryDNS. See error log.`);
+                console.error(new Date(), err);
+              }
+            )
+          }
         }
       }
     }
   }
 
   console.log(req.headers);
+  console.log(`${statusCode} ${rc}`)
   res.writeHead(statusCode, {"content-type": "text/plain"});
-  res.write(`${statusCode} ${req.url} ${req.socket.remoteAddress}\n`);
+  res.write(`${rc}\n${statusCode} ${req.url} ${req.socket.remoteAddress}\n`);
   Object.keys(req.headers).forEach(k => {
     res.write(`${k} : ${req.headers[k]}\n`);
   });
   res.end();
   
 }).listen(8084);
+
 
 
